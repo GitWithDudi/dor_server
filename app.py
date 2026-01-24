87e00c6e07dbb8235b3b0c87b958d67816ab4b15
GitWithDudi <dudi.gitar@gmail.com>
2026-01-25 00:34:23 +0200

    update app on route customer request

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from src.Routes.Routes_projects import projects_bp
+from src.Routes.Routes_customer_request import customer_request_bp
 from flask import Flask
 from flask_cors import CORS
 
@@ -16,6 +17,7 @@ CORS(app,resources={r"/*": {"origins": [
 ]}})
 
 app.register_blueprint(projects_bp)
+app.register_blueprint(customer_request_bp)
 
 if __name__ == '__main__':
      app.run(host='0.0.0.0', port=5000, debug=True)
