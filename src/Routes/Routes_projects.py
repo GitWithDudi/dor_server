44a5bca1028caf54bb0259348fa4918a0e395f2a
GitWithDudi <dudi.gitar@gmail.com>
2026-02-13 10:11:37 +0200

    Update import statement and add @jwt_required() decorator to create_project_route function

--- src/Routes/Routes_projects.py
+++ src/Routes/Routes_projects.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+from flask_jwt_extended import jwt_required
 from src.Controller.Controller__projects import get_projects, get_projects_by_category, create_project
 
 projects_bp = Blueprint('projects', __name__)
@@ -12,5 +13,6 @@ def projects_by_category(category):
     return get_projects_by_category(category)
 
 @projects_bp.route('/projects', methods=['POST'])
+@jwt_required()
 def create_project_route():
     return create_project()
